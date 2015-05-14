7a9ace6848923171f1e17b20f3b67115e0ba9a83
sovello <sovellohpmgani@gmail.com>
2015-05-13 21:12:45 -0400

    got some basic tests (addition, multiplication and equality) working

--- matrix_math.py
+++ matrix_math.py
@@ -1,34 +1,63 @@
 from exceptions import *
 
-class Matrix:
+class ShapeException(Exception):
+    pass
+
+
+class Matrix(object):
 
     # matrix properties
-    def __init__(self, matrix=[], num_rows = 0, num_cols = 0):
+    def __init__(self, matrix):
         self.matrix = matrix
-        self.num_rows = matrix[0]
-        self.num_cols = matrix[0][1]
+
+        # This initialization serves to distinguish Matrices from Vectors
+        if isinstance(self.matrix[0], list):
+            self.num_rows = len(self.matrix)
+            self.num_cols = len(self.matrix[0])
+        else:
+            self.num_rows = len(matrix)
+            self.num_cols = 0
 
 
     # get the shape (order) of the matrix
-    def shape(self, matrix):
-        if isinstance(matrix[0], list):
-            return len(matrix),len(matrix[0])
-        else:
-            return (len(matrix),)
+    def shape(self):
+        return self.num_rows, self.num_cols
 
+    def __getitem__(self, key):
+    # if key is of invalid type or value, the list values will raise the error
+        return self.matrix[key]
 
+    def __setitem__(self, key, value):
+        self.matrix[key] = value
+
+    def __len__(self):
+        return len(self.matrix)
+
+    def __eq__(self, other):
+        return self.matrix == other.matrix
+
+    def __iter__(self):
+        return iter(self.matrix)
+
+    def append(self, value):
+        self.values.append(value)
 
     # matrix addition
     def __add__(self, other):
-        # if self.shape(x) != self.shape(y):
-        #     raise ShapeException
-        # else:
-        #     return [ x[i]+y[i] for i in range(len(x))]
-
+        if self.shape() != other.shape():
+            raise ShapeException
+        else:
+            return Matrix([ [self.matrix[row][idx]+other.matrix[row][idx] for idx in range(len(other.matrix[0]))] for row in range(len(self.matrix))])
 
     # matrix multiplication, include multiplication by a scalar
     def __mul__(self, other):
-        pass
+        #if shape(matrix1)[1] != shape(matrix2)[0]:
+        if self.shape()[1] != other.shape()[0]:
+            raise ShapeException
+        else:
+            rotation = Matrix([[value[i] for value in other.matrix] for i in range(len(other.matrix[0]))])
+
+            return Matrix([[self.dot_product(self.matrix[i], rotation.matrix[j]) for j in range(len(rotation.matrix))] for i in range(len(self.matrix))])
 
 
     # matrix subtration
@@ -36,15 +65,12 @@ class Matrix:
         pass
 
 
-    # comparison for equality: when are two matrices equal?
-    def __eq__(self, other):
-        pass
-
-
     # rotate a matrix
     def rotation(self, matrix):
         pass
 
+    def dot_product(self, u,v):
+        return sum([ u[i]*v[j] for i, value in enumerate(v) for j,value2 in enumerate(u) if i==j])
 
     # create a matrix given size () and a function
     def create_matrix(self, size, function):
@@ -52,4 +78,7 @@ class Matrix:
 
 
 if __name__ == '__main__':
-    print("Voila")
+    mat = Matrix([[0, 1], [1, 0]])
+    A = [[2, 3], [1, 2]]
+    q = Matrix(A)
+    print( q * Matrix(A))
--- test_matrix_math.py
+++ test_matrix_math.py
@@ -14,16 +14,18 @@ Matrix([[1, 1, 1], [0, 0, 0]]) * Matrix([[1, 1], [2, 2], [3, 3]])
 Matrix([[0, 1], [1, 0]]) == Matrix([[1, 1], [0, 0]]) # results in False
 '''
 
-from Matrix import *
-from nosetests import raises
+from matrix_math import Matrix
+from nose.tools import raises
 
-matrix = Matrix()
-@raises(MatrixVectorException)
+#@raises(MatrixVectorException)
 def test_matrix_shape():
-    assert matrix.shape(Matrix([[0, 1], [1, 0]])) == (2,2)
+    matrix = Matrix(B)
+    assert Matrix(B).shape() == (3, 3)
 
 def test_magic_add():
-    pass
+    mat1 = Matrix(A)
+    matb = Matrix(B)
+    assert  mat1 + matb == Matrix([[2, 2, 3],[4, 6, 6],[7, 8, 10]])
 
 def test_magic_subtract():
     pass
@@ -31,8 +33,26 @@ def test_magic_subtract():
 def test_magic_equal():
     pass
 
+A = [[1, 0, 0],
+     [0, 1, 0],
+     [0, 0, 1]]
+B = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+C = [[1, 2],
+     [2, 1],
+     [1, 2]]
+D = [[1, 2, 3],
+     [3, 2, 1]]
+
+F = [3, 4]
+
 def test_magic_multiply():
-    pass
+    #assert Matrix(A) * Matrix(B) == Matrix(B)
+    assert Matrix(B) * Matrix(C) == Matrix([[8, 10], [20, 25], [32, 40]])
+    assert Matrix(C) * Matrix(D) == Matrix([[7, 6, 5], [5, 6, 7], [7, 6, 5]])
+    assert Matrix(D) * Matrix(C) == Matrix([[8, 10], [8, 10]])
+    assert Matrix(C) * Matrix(F) == Matrix([8, 11, 11])
 
 def test_matrix_rotation():
     pass
